2c850dcf9aa348ed95a44e185605536f89d955db
shakti <shakti@localhost.localdomain>
2022-04-19 19:59:30 +0530

    added files

---
+++ script.js
@@ -0,0 +1,7 @@
+navbar=document.querySelector(".navbar")
+menubar=document.querySelector("#menu-bar")
+menubar.onclick=()=> {
+    navbar.classList.toggle("nav-active")
+    menubar.classList.toggle("fa-xmark")
+
+}
